f3004f1f287755b6f475d1eb8262a4ba71a9e759
Haykaybe3 <haykaybee3@gmail.com>
2025-09-19 15:17:39 +0100

    data update

--- app/config/aa.config.ts
+++ app/config/aa.config.ts
@@ -3,7 +3,7 @@
 import { sepolia } from "viem/chains";
 
 export const AA_CHAIN = sepolia;
-export const BUNDLER_URL = `https://api.pimlico.io/v2/${AA_CHAIN.id}/rpc?apikey=${process.env.NEXT_PUBLIC_PIMLICO_API_KEY}`;
-export const PAYMASTER_URL = `https://api.pimlico.io/v2/${AA_CHAIN.id}/rpc?apikey=${process.env.NEXT_PUBLIC_PIMLICO_API_KEY}`;
+export const BUNDLER_URL = `https://api.pimlico.io/v2/${AA_CHAIN.id}/rpc?apikey=${process.env.NEXT_PIMLICO_API_KEY}`;
+export const PAYMASTER_URL = `https://api.pimlico.io/v2/${AA_CHAIN.id}/rpc?apikey=${process.env.NEXT_PIMLICO_API_KEY}`;
 
 
